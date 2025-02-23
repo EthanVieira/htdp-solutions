6a1d3f214e1429d6ac76f686bdc7aecefc20aac9
Ethan Vieira <ethanvieira@gmail.com>
2025-02-23 13:34:32 -0600

    Create 009.rkt

---
+++ ch1/009.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |009|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+(define in #true)
+
+(cond [(number? in) (abs in)]
+      [(string? in) (string-length in)]
+      [(image? in) (* (image-width in) (image-height in))]
+      [(boolean? in) (if in 10 20)])
